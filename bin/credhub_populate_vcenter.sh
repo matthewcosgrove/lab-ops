56a5f7f90ae6ce93ce276c3e481dce204a9f86b9
matthewcosgrove <mc.matthew.cosgrove@gmail.com>
2020-07-16 21:40:22 +0000

    Rely on CredHub for vcenter config

--- bin/credhub_populate_vcenter.sh
+++ bin/credhub_populate_vcenter.sh
@@ -6,9 +6,10 @@ crednames=( \
   vcenter_username \
   vcenter_password
 )
+credpath_prefix="/concourse/main/"
 for credname in "${crednames[@]}"
 do
-  credpath="/concourse/main/${credname}"
+  credpath="${credpath_prefix}${credname}"
   if ! credhub get -n "${credpath}" > /dev/null 2>&1;then
     echo "Please enter cred for: ${credpath}"
     if [[ "${credname}" == *password ]];then
@@ -20,3 +21,13 @@ do
     echo "${credpath} already created, skipping.."
   fi
 done
+
+credhub set -n "${credpath_prefix}"govc_url -t value -v "${GOVC_URL}"
+credhub set -n "${credpath_prefix}"govc_datacenter -t value -v "${GOVC_DATACENTER}"
+credhub set -n "${credpath_prefix}"govc_cluster -t value -v "${GOVC_CLUSTER}"
+credhub set -n "${credpath_prefix}"govc_network -t value -v "${GOVC_NETWORK}"
+credhub set -n "${credpath_prefix}"govc_vm_folder_path -t value -v "${GOVC_VM_FOLDER_PATH}"
+
+credhub set -n "${credpath_prefix}"vcenter_primary_datastore -t value -v "${VCENTER_PRIMARY_DATASTORE}"
+credhub set -n "${credpath_prefix}"vcenter_resource_pool_name -t value -v "${VCENTER_RESOURCE_POOL_NAME}"
+credhub set -n "${credpath_prefix}"vcenter_folder_name -t value -v "${VCENTER_FOLDER_NAME}"
