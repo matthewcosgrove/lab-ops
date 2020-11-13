986dbf8924f9dd5615b9c54cebed4dab6e69e9f5
Matthew Cosgrove <mc.matthew.cosgrove@gmail.com>
2020-11-13 14:39:05 +0000

    Add govc_folder_path_prefix

--- bin/credhub_populate_vcenter.sh
+++ bin/credhub_populate_vcenter.sh
@@ -23,6 +23,7 @@ credhub_set govc_cluster "${GOVC_CLUSTER}"
 credhub_set govc_network "${GOVC_NETWORK}"
 credhub_set govc_folder "${GOVC_FOLDER}"
 credhub_set govc_vm_folder_path "${GOVC_FOLDER}" # keeping for backwards compatibility
+credhub_set govc_folder_path_prefix "/${GOVC_DATACENTER}/vm/" # path prefix expected by govc useful for combining with VCENTER_FOLDER_NAME_RELATIVE_PATH because GOVC_FOLDER=govc_folder_path_prefix/VCENTER_FOLDER_NAME_RELATIVE_PATH
 
 credhub_set vcenter_primary_datastore "${GOVC_DATASTORE}"
 credhub_set vcenter_resource_pool_name "${VCENTER_RESOURCE_POOL_NAME}"
