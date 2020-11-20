1cf09e59888a0d9e32f74c4b8a09c5f3b1949c4d
Matthew Cosgrove <mc.matthew.cosgrove@gmail.com>
2020-11-20 11:53:27 +0000

    Naming convention for certain vcenter credhub vars

--- bin/credhub_populate_vcenter.sh
+++ bin/credhub_populate_vcenter.sh
@@ -21,10 +21,16 @@ credhub_set vcenter_password "${GOVC_PASSWORD}"
 credhub_set govc_datacenter "${GOVC_DATACENTER}"
 credhub_set govc_cluster "${GOVC_CLUSTER}"
 credhub_set govc_network "${GOVC_NETWORK}"
-credhub_set govc_folder "${GOVC_FOLDER}"
-credhub_set govc_vm_folder_path "${GOVC_FOLDER}" # keeping for backwards compatibility
-credhub_set govc_folder_path_prefix "/${GOVC_DATACENTER}/vm/" # path prefix expected by govc useful for combining with VCENTER_FOLDER_NAME_RELATIVE_PATH because GOVC_FOLDER=govc_folder_path_prefix/VCENTER_FOLDER_NAME_RELATIVE_PATH
 
 credhub_set vcenter_primary_datastore "${GOVC_DATASTORE}"
-credhub_set vcenter_resource_pool_name "${VCENTER_RESOURCE_POOL_NAME}"
-credhub_set vcenter_folder_name "${VCENTER_FOLDER_NAME_RELATIVE_PATH}"
+credhub_set vcenter_resource_pool_name "${VCENTER_RESOURCE_POOL_NAME}" # deprecated, currently keeping for backwards compatibility, new convention is to use specific prefix to differentiate it
+credhub_set bucc_vcenter_resource_pool_name "${VCENTER_RESOURCE_POOL_NAME}"
+credhub_set vcenter_folder_name "${VCENTER_FOLDER_NAME_RELATIVE_PATH}" # deprecated, currently keeping for backwards compatibility, new convention is to use specific prefix to differentiate it
+credhub_set bucc_vcenter_folder_name_relative_path "${VCENTER_FOLDER_NAME_RELATIVE_PATH}"
+
+govc_folder_prefix="/${GOVC_DATACENTER}/vm/"
+credhub_set govc_folder_path_prefix "${govc_folder_prefix}" # path prefix expected by govc useful for combining with VCENTER_FOLDER_NAME_RELATIVE_PATH because GOVC_FOLDER=govc_folder_path_prefix/VCENTER_FOLDER_NAME_RELATIVE_PATH
+GOVC_FOLDER="${govc_folder_prefix}${VCENTER_FOLDER_NAME_RELATIVE_PATH}"
+credhub_set govc_folder "${GOVC_FOLDER}" # deprecated, currently keeping for backwards compatibility, new convention is to use specific prefix to differentiate it
+credhub_set govc_vm_folder_path "${GOVC_FOLDER}" # deprecated, currently keeping for backwards compatibility, new convention is to use specific prefix to differentiate it
+credhub_set bucc_govc_folder "${GOVC_FOLDER}"
